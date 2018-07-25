d0d4b46bf412bcb693db1f9a2137cc3bc7bb60f3
Дарья Чернова <dariashvartz@mail.ru>
2018-07-25 20:12:17 +0300

    adding NotFound component

---
+++ src/components/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="has-text-centered">
+        <h1 class="title">Sorry. Page not found :(</h1>
+        <p>Use the navigation links above to navigate between the product and cart screens</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NotFound',
+}
+</script>
+
+<style scoped>
+</style>
